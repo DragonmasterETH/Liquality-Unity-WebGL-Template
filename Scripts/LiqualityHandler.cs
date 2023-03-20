dd7f1a4c0b93871a317ca4d7b0e541b7876904dc
Slula <36647367+Slula@users.noreply.github.com>
2023-03-19 22:45:31 -0400

    Upload LiqualityHandler script

---
+++ Scripts/LiqualityHandler.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class LiqualityHandler : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void GetSelectedAccount();
+
+    [DllImport("__Internal")]
+    private static extern void GetNfts(string wallet, int chain);
+
+    [DllImport("__Internal")]
+    private static extern void Mint721(string contractAddress, string recipient, int id, int amount, int chainId, string pk);
+
+    [DllImport("__Internal")]
+    private static extern void Mint1155(string contractAddress, string recipient, int id, int amount, int chainId, string pk);
+
+    [DllImport("__Internal")]
+    private static extern void Create721(string uri, int chain, string pk);
+
+    [DllImport("__Internal")]
+    private static extern void Create1155(string uri, int chain, string pk);
+
+    [DllImport("__Internal")]
+    private static extern void TransferNFT(string contractAddress, string receiver, string tokenIDs, string amounts, int chain, string pk);
+
+    [DllImport("__Internal")]
+    private static extern void ListTokens(string wallet, int id);
+
+    [DllImport("__Internal")]
+    private static extern void GetToken(string contract, string wallet, int chain);
+
+    [DllImport("__Internal")]
+    private static extern void Setup(string alchemyKey, string etherscanKey);
+
+    public void GetWallet(string resp)
+    {
+        string address = resp.Split(',')[0];
+        string privatekey = resp.Split(',')[1];
+
+        //Do something with the keys
+    }
+
+
+    public void GetNFTs(string nftJson)
+    {
+        // This returns all nfts in a json string
+    }
+
+    public void MintERC721Hash(string hash)
+    {
+        // this returns the hash from minting the NFT
+    }
+
+    public void MintERC1155Hash(string hash)
+    {
+        // this returns the hash from minting the NFT
+    }
+
+    public void CreateERC721Hash(string hash)
+    {
+        // this returns the hash from creating the NFT collection
+    }
+
+    public void CreateERC1155Hash(string hash)
+    {
+        // this returns the hash from creating the NFT collection
+    }
+
+    public void TransferNFTHash(string hash)
+    {
+        // this returns the hash from transferring NFTs
+    }
+
+    public void ListAllTokens(string tokensJson)
+    {
+        // this returns a list of all owned tokens in a json string
+    }
+
+    public void GetTokenBalance(string bal)
+    {
+        // this returns the balance of the rewuested token
+    }
+}
